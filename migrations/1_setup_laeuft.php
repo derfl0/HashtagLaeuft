8726a1147d51f913449a0c39770aff43cd98f34e
Florian Bieringer <florian.bieringer@uni-passau.de>
2015-04-21 13:01:44 +0200

    migration

---
+++ migrations/1_setup_laeuft.php
@@ -0,0 +1,23 @@
+<?php
+
+class SetupLaeuft extends DBMigration {
+
+    function up() {
+
+        // Create printer table
+        DBManager::get()->exec("CREATE TABLE `laeuft` (
+  `laeuft_id` varchar(32) NOT NULL DEFAULT '',
+  `user_id` varchar(32) NOT NULL DEFAULT '',
+  PRIMARY KEY (`laeuft_id`),
+  KEY (`user_id`)
+)");
+
+        StudipAutoloader::addAutoloadPath(__DIR__ . '/../models');
+        ToDo::expireTableScheme();
+    }
+
+    function down() {
+        DBManager::get()->exec("DROP TABLE IF EXISTS `laeuft`");
+    }
+
+}
