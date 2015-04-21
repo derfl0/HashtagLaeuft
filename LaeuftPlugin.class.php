c3dbc209a3bb22dec713549ae45f819acc52342f
Florian Bieringer <florian.bieringer@uni-passau.de>
2015-04-21 12:55:36 +0200

    Hashtag

--- LaeuftPlugin.class.php
+++ LaeuftPlugin.class.php
@@ -4,8 +4,10 @@ class LaeuftPlugin extends StudIPPlugin implements SystemPlugin {
 
     public function __construct() {
         parent::__construct();
-        StudipTransformFormat::addStudipMarkup('LaeuftPlugin', '\[läuft\]', NULL, 'LaeuftPlugin::init');
-        StudipFormat::addStudipMarkup('LaeuftPlugin', "\[läuft:(\w){32}\]", null, 'LaeuftPlugin::markup');
+        PageLayout::addScript($this->getPluginURL() . '/laeuft.js');
+        PageLayout::addStylesheet($this->getPluginURL() . '/laeuft.css');
+        StudipTransformFormat::addStudipMarkup('LaeuftPlugin', '#läuft', NULL, 'LaeuftPlugin::init');
+        StudipFormat::addStudipMarkup('LaeuftPlugin', "#läuft:(\w){32}", null, 'LaeuftPlugin::markup');
 
         if (Request::submitted('laeuft')) {
             $parameter = array(Request::get('laeuft'), $GLOBALS['user']->id);
@@ -18,15 +20,16 @@ class LaeuftPlugin extends StudIPPlugin implements SystemPlugin {
     }
 
     public static function init($markup, $matches, $contents) {
-        return "[läuft:" . md5(uniqid("läuft")) . "]";
+        return "#läuft:" . md5(uniqid("läuft"));
     }
 
     public static function markup($markup, $matches, $contents) {
         $md5 = substr($matches[0], 7, 32);
+        $parameter = array(md5($md5), $GLOBALS['user']->id);
+        $count = DBManager::get()->fetchColumn('SELECT count(*) FROM laeuft WHERE laeuft_id = ? AND user_id != ?', $parameter);
+        $me = DBManager::get()->execute('SELECT 1 FROM laeuft WHERE laeuft_id = ? AND user_id = ?', $parameter) ? 'laeuft_bei_dir' : '';
         
-        $count = DBManager::get()->fetchColumn('SELECT count(*) FROM laeuft WHERE laeuft_id = ?', array(md5($md5)));
-        
-        return sprintf(dgettext('laeuft', 'Läuft bei %s Personen'), $count) . ' ' . Studip\LinkButton::create('Läuft!', '?laeuft=' . md5($md5));
+        return sprintf(dgettext('laeuft', '<a class="laeuft '.$me.'" href="'.URLHelper::getLink('', array('laeuft' => md5($md5))).'">#läuft</a> bei<span> dir und</span> %s Personen'), $count);
     }
 
 }
